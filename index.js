d41f69fc570b5b5d6024e25b3e53f696b070aca3
derekling <derekling@Lings-Air.fritz.box>
2020-04-26 03:09:36 +0200

    Refactor autocomplete

--- index.js
+++ index.js
@@ -1,70 +1,30 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '760378ac',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-
-}
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown is-hide">
-  <div class="dropdown-memu">
-    <div class="dropdown-content results">
-    </div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-
-const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.add('is-hide');
-    return;
-  }
-
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.remove('is-hide');
-  for(let movie of movies) {
-    const option = document.createElement('a');
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-    
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
+    return `
       <img src="${imgSrc}" />
-      ${movie.Title}
+      ${movie.Title} (${movie.Year})
     `;
-    option.addEventListener('click', () => {
-      dropdown.classList.add('is-hide');
-      input.value = movie.Title;
-      onMovieSelect(movie);
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '760378ac',
+        s: searchTerm
+      }
     });
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.add('is-hide');
+  
+    if (response.data.Error) {
+      return [];
+    }
+    return response.data.Search;
   }
 });
 
